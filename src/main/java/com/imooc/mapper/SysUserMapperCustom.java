39e81aa7a0e1f750c66e195da770befb9ec2321c
westboycome <476878873@qq.com>
2018-07-03 00:29:16 +0800

    删除缓存

--- src/main/java/com/imooc/mapper/SysUserMapperCustom.java
+++ src/main/java/com/imooc/mapper/SysUserMapperCustom.java
@@ -3,8 +3,9 @@ package com.imooc.mapper;
 import java.util.List;
 
 import com.imooc.pojo.SysUser;
+import com.imooc.utils.MyMapper;
 
-public interface SysUserMapperCustom {
+public interface SysUserMapperCustom extends MyMapper<SysUser> {
 
 	List<SysUser> queryUserSimplyInfoById(String id);
 }
